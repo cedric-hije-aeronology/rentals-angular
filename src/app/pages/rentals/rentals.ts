75d970e969ada0be318f5a7397469167a86b15c6
Cedric Vincent H <cedric.hije@aeronology.travel>
2026-05-20 21:16:49 +0800

    Initial files

---
+++ src/app/pages/rentals/rentals.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rentals',
+  imports: [],
+  templateUrl: './rentals.html',
+  styleUrl: './rentals.scss',
+})
+export class Rentals {}
